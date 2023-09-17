dc54e3c5bd034daf51bb8c41f7a7ae9dbe67b72b
Victor <b3m.v@yandex.ru>
2023-09-18 01:01:51 +0300

    Initial commit

--- StaryBoardTest2/ViewController.swift
+++ StaryBoardTest2/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var skyImageView: UIImageView!
+    @IBOutlet weak var changeButton: UIButton!
+    private var isSun: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        skyImageView.image = UIImage(systemName: "moon")
+        skyImageView.tintColor = .gray
+        
+        //changeButton.tintColor = UIColor.green
     }
-
-
+    
+    
+    @IBAction func buttonDidTap(_ sender: Any) {
+        if isSun {
+            skyImageView.image = UIImage(systemName: "moon")
+        } else {
+            skyImageView.image = UIImage(systemName: "sun.max")
+        }
+        isSun.toggle()
+    }
+    
+    @IBAction func buttonTouchDown() {
+        print("Touched Down")
+    }
+    
 }
 
